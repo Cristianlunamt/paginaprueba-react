b60399a3ea0cfc6b59aadce3524b83050fc50115
Cristian Luna Mateus <cristianlunamateus@gmail.com>
2024-11-19 20:52:01 -0500

    Pagina

--- mis proyectos react/pagina-prueba/src/App.js
+++ mis proyectos react/pagina-prueba/src/App.js
@@ -1,22 +1,16 @@
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export default function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
-
+function MyButton() {
   return (
-    <ul>{listItems}</ul>
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
   );
 }
